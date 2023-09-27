b24704d044233371a71789c6857a85a45f42eaf1
Shreyas Mocherla <shreyas@shreyasm.com>
2023-09-27 18:57:19 -0500

    Main

--- main.cpp
+++ main.cpp
@@ -119,6 +119,7 @@ int main(int argc, char* argv[])
             {
                 stepAction = complete;
                 curRunning->state = done;
+                break;
             }
             else{
                 stepAction = continueRun;
